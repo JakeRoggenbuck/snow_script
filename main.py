c5cfd19c626cac03540c4c6aa1963e3694101d72
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-06-19 01:07:50 -0700

    Make code cleaner

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("filename", type=str)
 args = parser.parse_args()
 
-memory = []
+mem = []
 pointer = 0
 
 with open(args.filename, 'r') as f:
@@ -15,28 +15,30 @@ with open(args.filename, 'r') as f:
 for line in lines:
     c = shlex.split(line)
     if c[0] == "input":
-        memory.append(input(c[1]))
+        mem.append(input(c[1]))
     elif c[0] == "input#":
-        memory[int(c[1])] = input(c[2])
+        mem[int(c[1])] = input(c[2])
     elif c[0] == "int&":
-        memory[int(c[1])] = int(memory[int(c[1])])
+        mem[int(c[1])] = int(mem[int(c[1])])
     elif c[0] == "str&":
-        memory[int(c[1])] = str(memory[int(c[1])])
+        mem[int(c[1])] = str(mem[int(c[1])])
     elif c[0] == "int":
-        memory.append(int(memory[int(c[1])]))
+        mem.append(int(mem[int(c[1])]))
     elif c[0] == "str":
-        memory.append(str(memory[int(c[1])]))
+        mem.append(str(mem[int(c[1])]))
     elif c[0] == "calc":
-        memory.append(eval(str(memory[int(c[1])]) + c[2] + str(memory[int(c[3])])))
+        mem.append(eval(
+            str(mem[int(c[1])]) + c[2] + str(mem[int(c[3])])))
     elif c[0] == "calc#":
-        memory[int(c[1])] = eval(str(memory[int(c[2])]) + c[3] + str(memory[int(c[4])]))
+        mem[int(c[1])] = eval(
+            str(mem[int(c[2])]) + c[3] + str(mem[int(c[4])]))
     elif c[0] == "val":
-        memory.append(c[1])
+        mem.append(c[1])
     elif c[0] == "val#":
-        memory[int(c[1])] = c[2]
+        mem[int(c[1])] = c[2]
     elif c[0] == "out":
-        print(memory[int(c[1])])
+        print(mem[int(c[1])])
     elif c[0] == "push":
-        memory.append(str(memory[int(c[1])]) + str(memory[int(c[2])]))
+        mem.append(str(mem[int(c[1])]) + str(mem[int(c[2])]))
     elif c[0] == "push#":
-        memory[int(c[1])] = str(memory[int(c[2])]) + str(memory[int(c[3])])
+        mem[int(c[1])] = str(mem[int(c[2])]) + str(mem[int(c[3])])
