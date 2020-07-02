0363ff0d28a13a464d550f913c19764ee15af45a
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-07-01 21:02:56 -0700

    Add int test

--- tests/test_ssc.py
+++ tests/test_ssc.py
@@ -32,6 +32,33 @@ class testCommands(unittest.TestCase):
             mem[int(c[int(1)])] = ssc.str_(mem,c)
             self.assertEqual(mem, ans)
 
+    def test_int(self):
+        import src.ssc
+
+        ssc = src.ssc.commands()
+        ints = [
+            [["123", "456", "789"],["int", "0"],["123", "456", "789", 123]],
+            [["4", "5", "6"],["int", "1"],["4", "5", "6", 5]],
+        ]
+        for _int in ints:
+            mem = _int[0]
+            c = _int[1]
+            ans = _int[2]
+            ssc.int_(mem,c)
+            self.assertEqual(mem, ans)
+
+        ints = [
+            [["123", "456", "789"],["str&", "0"],[123, "456", "789"]],
+            [["4", "5", "6"],["str&", "1"],["4", 5, "6"]],
+        ]
+        for _int in ints:
+            mem = _int[0]
+            c = _int[1]
+            ans = _int[2]
+            mem[int(c[int(1)])] = ssc.int_(mem,c)
+            self.assertEqual(mem, ans)
+
+
 
 if __name__ == "__main__":
     unittest.main()
